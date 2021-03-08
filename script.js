f5a2d235a2ff8229966022f41d382377bdb5fcf6
-  ray  - <51856916+raymarquez@users.noreply.github.com>
2021-03-07 21:09:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var margin = {top:10, right:30, bottom:30, left:40},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-var svg = d3.select("#my_dataviz2")
+var svg = d3.select("#my_dataviz")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
